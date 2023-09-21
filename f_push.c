743838062cc1fb61e8abbbd1cd2e6c62e9825704
IBRAHIMAlla <brahimalla973@gmail.com>
2023-09-21 12:53:33 -0700

    f_push.c

--- f_push.c
+++ f_push.c
@@ -7,19 +7,35 @@
 */
 void f_push(stack_t **head, unsigned int num)
 {
-	stack_t *ptr = malloc(sizeof(stack_t));
+	int m, y = 0, fl = 0;
 
-	if (head == NULL)
-		return;
-	if (ptr == NULL)
+	if (bus.discu)
 	{
-		fprintf(stderr, "Error: malloc failed");
-		exit(EXIT_FAILURE);
-	}
-	ptr->n = num;
-	ptr->next = *head;
-	ptr->prev = NULL;
-	if (*head)
-		(*head)->prev = ptr;
-	*head = ptr;
+		if (bus.discu[0] == '-')
+			y++;
+		while (bus.discu[y] != '\0')
+		{
+			if (bus.discu[y] > 57 || bus.discu[y] < 48)
+			{
+				fl = 1; 
+			}
+			y++;
+		}
+		if (fl == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", num);
+			fclose(bus.reg);
+			free(bus.text);
+			stack_free(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", num);
+		fclose(bus.reg);
+		free(bus.text);
+		stack_free(*head);
+		exit(EXIT_FAILURE); }
+	m = atoi(bus.discu);
+	if (bus.lifi == 0)
+		add_node(head, m);
+	else
+		add_queue(head, m);
 }
